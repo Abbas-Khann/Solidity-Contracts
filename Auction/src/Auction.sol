8a7745bd8b1f82cd00744606b0ba13962fac4445
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2023-04-15 17:19:28 +0800

    Added state vars and events

--- Auction/src/Auction.sol
+++ Auction/src/Auction.sol
@@ -3,18 +3,41 @@ pragma solidity ^0.8.13;
 import "../Interfaces/IERC721.sol";
 
 contract Auction {
+    // event to emit when a bid is made
+    event bid_made(address indexed sender, uint256 amount);
+    // event to emit when the auction is ended
+    event auction_ended(address indexed winner, uint256 ending_timestamp);
+    // nft contract address
     IERC721 private nft_contract;
-    uint256 nft_id;
+    // token_id for the nft
+    uint256 private token_id;
+    // seller address
     address private seller;
+    // highest_bidder for the NFT
+    uint256 private highest_bidder;
+    // starting bid for the NFT
     uint256 private starting_bid;
+    // highest bid for each NFT
+    uint256 private highest_bid;
+    // bool to check if bidding is still active
     bool private bidding_active;
-    uint64 private bid_timespan;
+    // timespan of the bid
+    uint64 private constant bid_timespan = 24 hours;
+    // array of all bidders
+    address[] private bidders;
+    // mapping to track the amount bid by each bidder
+    mapping(address => uint256) public amount_bid;
 
-    constructor(address _nft_contract, uint256 _nft_id) payable {
+    constructor(address _nft_contract, uint256 _token_id) payable {
+        // initializing the nft contract address
         nft_contract = IERC721(_nft_contract);
-        nft_id = _nft_id;
+        // setting up the token_id
+        token_id = _token_id;
+        // initializing the seller as the auction contract owner
         seller = msg.sender;
+        // initializing the starting_bid to be the amount sent by the seller
         starting_bid = msg.value;
+        // setting the bidding_active bool to true after contract is deployed
         bidding_active = true;
     }
 }
