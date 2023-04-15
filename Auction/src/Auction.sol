fad526a3acf1123f635d6a585f4c48081ad30be6
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2023-04-15 15:55:48 +0800

    Added state vars and constructor

--- Auction/src/Auction.sol
+++ Auction/src/Auction.sol
@@ -1,4 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import "../Interfaces/IERC721.sol";
 
-contract Auction {}
+contract Auction {
+    IERC721 private nft_contract;
+    uint256 nft_id;
+    address private seller;
+    uint256 private starting_bid;
+    bool private bidding_active;
+    uint64 private bid_timespan;
+
+    constructor(address _nft_contract, uint256 _nft_id) payable {
+        nft_contract = IERC721(_nft_contract);
+        nft_id = _nft_id;
+        seller = msg.sender;
+        starting_bid = msg.value;
+        bidding_active = true;
+    }
+}
